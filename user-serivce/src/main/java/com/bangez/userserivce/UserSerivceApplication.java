5e2d10a48cb10f417105153c9a6ee37b1cd084ba
JWOOSAGI <ddgfg3234@naver.com>
2024-07-23 15:09:57 +0900

    first

---
+++ user-serivce/src/main/java/com/bangez/userserivce/UserSerivceApplication.java
@@ -0,0 +1,13 @@
+package com.bangez.userserivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSerivceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserSerivceApplication.class, args);
+    }
+
+}
